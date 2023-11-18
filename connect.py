3b3401aa5eab1c0fb12f778f704a63dc39d879b4
aureateAnatidae <34411014+aureateAnatidae@users.noreply.github.com>
2023-11-18 16:46:33 -0700

    Beta connect()

---
+++ connect.py
@@ -0,0 +1,31 @@
+from brainflow.board_shim import BoardShim, BoardIds, BrainFlowInputParams
+
+
+class stream:
+    '''
+    Streaming object returning data from BoardShim on method get_data() call
+    
+    Attributes
+    ----------
+    params : ???
+        set of parameters required for the OpenBCI to connect
+    params.serial_port : str
+        computer port to access the OpenBCI ganglion hardware - changes based on computer and OS
+    board_id : int
+        ganglion hardware ID
+    '''
+    def __init__(self):
+        params = BrainFlowInputParams()
+        params.serial_port = "COM3"
+        board_id = BoardIds.GANGLION_BOARD.value
+        self.board = BoardShim(board_id, params)
+        self.board.prepare_session()
+        self.board.start_stream()
+    
+    def __exit__(self):
+        self.board.stop_stream()
+        self.board.release_session()
+
+    def get_data(self):
+        data = self.board.get_board_data()
+        return data
